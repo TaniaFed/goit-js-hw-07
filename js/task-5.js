56498777ecab1c7cc0925ab6869f84591af2a367
TaniaFed <ktv.poshta@gmail.com>
2024-04-07 18:49:34 -0400

    hw-7 (task-5)

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,26 @@
+const changeColorbtn = document.querySelector(`button.change-color`);
+
+const span = document.querySelector(`span.color`);
+
+changeColorbtn.addEventListener("click", changeColor);
+
+function changeColor() {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  span.textContent = color;
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+
+
+
+
+
+
+
+
