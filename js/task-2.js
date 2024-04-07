a14f980887d39bff28e675f480ad23fa4d200ddf
TaniaFed <ktv.poshta@gmail.com>
2024-04-07 18:08:39 -0400

    hw-7 (task-4)

--- js/task-2.js
+++ js/task-2.js
@@ -38,6 +38,7 @@ const pictures = images
 
 imgList.insertAdjacentHTML("afterbegin", pictures);
 
+// imgList.innerHTML = pictures;
 
 
 
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,30 @@
+const sendForm = document.querySelector("form");
+
+sendForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    
+    event.preventDefault();
+
+    const email = sendForm.elements.email.value.trim();
+    const password = sendForm.elements.password.value.trim();
+  
+    if (email === "" || password === "") {
+        alert(`All form fields must be filled in`);
+        return;
+    }
+
+    const formInfo = {
+        email,
+        password
+    };
+
+    console.log(formInfo);
+  
+    sendForm.reset();
+};
+  
+
+
+
 
