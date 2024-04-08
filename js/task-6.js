0a81382b850ab244a63f90f9da0788456ae7bf8b
TaniaFed <ktv.poshta@gmail.com>
2024-04-07 21:20:04 -0400

    hw 7(task-6)

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,37 @@
+const input = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", createBoxes);
+
+destroyBtn.addEventListener("click", destroyBoxes);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+function createBoxes() {
+  const amount = Number(input.value);
+
+  if (amount >= 1 && amount <= 100) {
+    boxesContainer.innerHTML = "";
+  
+    for (let i = 0; i < amount; i++) {
+      const box = document.createElement("div");
+      box.style.width = `${30 + i * 10}px`;
+      box.style.height = `${30 + i * 10}px`;
+      box.style.backgroundColor = getRandomHexColor();
+      boxesContainer.appendChild(box);
+    };
+
+    input.value = "";
+  };
+
+};
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+};
